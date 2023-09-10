4609e84905e99de6a4eae8c409bc2ad0a5c2be7c
Guru <shubhamka13@gmail.com>
2023-09-10 16:21:04 +0530

    create project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        this is a class base component
+      </div>
+    )
+  }
+}
